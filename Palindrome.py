bd001351c795af7b9f6c84663574009104e21f9c
Kenneth Githambo <mrgithambo@gmail.com>
2020-04-03 21:39:10 +0300

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,20 +1,20 @@
-string=str(input("Enter a string\n"))
+#Palindrome.py By Kenneth Githambo 
+#palindrome is a string that when reversed its still the same 
 
-def ReverseFunction(string):
+
+string=str(input("Enter a string\n")) 
+
+def ReverseFunction(string):#this function reverses the string charcters 
 	return string[::-1]
 
-
-def PalindromeCheck(string):
+def PalindromeCheck(string):#This fucntion compares the rerversed string with the original String
 	rev=ReverseFunction(string)
 	print("original string is -->",string)
 	print("Reversed string is -->",rev)
-
 	
 	if rev==string:
 		print(string,"is a palindrome")
 	else:
-		print(string,"is not a palindrome")
-
-		
+		print(string,"is not a palindrome")		
 
 print(PalindromeCheck(string))
