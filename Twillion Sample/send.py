fee0392244640f38f842c39fc7211158df2425b9
unknown <gdidder10@gmail.com>
2020-09-23 00:16:27 +0300

    twilio update

--- Twillion Sample/send.py
+++ Twillion Sample/send.py
@@ -1,11 +1,14 @@
 from twilio.rest import Client
 
-TWILIO_ACCOUNT_SID="ACb99b4b21ae84dd31d10024a17bd25d8e"
-TWILIO_AUTH_TOKEN="a4fb0e3745010009fabcec5bffa7f41a"
+TWILIO_ACCOUNT_SID="your TWILIO_ACCOUNT_SID"
+TWILIO_AUTH_TOKEN="Your TWILIO_AUTH_TOKEN"
+
+mytwillio_number="your twilio number"
+sms="Your message"
 
 client=Client(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN)
 client.messages.create(
-				to="+254704478977",
-				from_="+12072227121",
-				body="Khali test "
+				to='input number[s]'
+				from_=mytwillio_number,
+				body=sms
 				)
